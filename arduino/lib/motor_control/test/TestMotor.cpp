64d5158fc025009f916f411b356a5039cca503ca
jonnydark <jonny@jonnydark.com>
2014-03-23 14:12:11 +0000

    Added motor control lib. Renamed output from 'firmware' to 'artemis_arduino'

---
+++ arduino/lib/motor_control/test/TestMotor.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE MotorTest
+#include <boost/test/unit_test.hpp>
+#include "Motor.h"
+
+
+BOOST_AUTO_TEST_SUITE(BlinkerTests)
+
+BOOST_AUTO_TEST_CASE(TestConstruction)
+{
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
