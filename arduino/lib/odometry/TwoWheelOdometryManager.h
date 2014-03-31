bb2b0f0a68d1f0128552305bd0537e39b9062d57
jonnydark <jonny@jonnydark.com>
2014-03-31 23:32:03 +0100

    OdometryManager is now an interface

---
+++ arduino/lib/odometry/TwoWheelOdometryManager.h
@@ -0,0 +1,12 @@
+#ifndef TWO_WHEEL_ODOMETRY_MANAGER_H
+#define TWO_WHEEL_ODOMETRY_MANAGER_H
+
+class TwoWheelOdometryManager : public OdometryManager
+{
+public:
+
+private:
+    /* data */
+};
+
+#endif /* end of include guard: TWO_WHEEL_ODOMETRY_MANAGER */
--- arduino/lib/odometry/test/OdometryTests.cpp
+++ arduino/lib/odometry/test/OdometryTests.cpp
@@ -2,11 +2,12 @@
 #define BOOST_TEST_MODULE Odometry_Tests
 #include <boost/test/unit_test.hpp>
 #include "OdometryManager.h"
+#include "TwoWheelOdometryManager.h"
 
 BOOST_AUTO_TEST_SUITE(TestOdometry)
 
 BOOST_AUTO_TEST_CASE(TestOdometryManagerContruction) {
-    OdometryManager odoManager;
+    OdometryManager* odoManager = new TwoWheelOdometryManager;
 }
     
 BOOST_AUTO_TEST_SUITE_END()
