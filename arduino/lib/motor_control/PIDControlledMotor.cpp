dbfa0204f4eecb2f47e81bb169fa825599504ad4
jonnydark <jonny@jonnydark.com>
2014-04-26 19:15:35 +0100

    Wrote a PID - still not passing tests though

--- arduino/lib/motor_control/PIDControlledMotor.cpp
+++ arduino/lib/motor_control/PIDControlledMotor.cpp
@@ -4,9 +4,29 @@
 
 PIDControlledMotor::PIDControlledMotor
     (const unsigned int wheelRadius, const Motor& motor, Encoder* const encoder)
-    : m_wheelCircumference(2 * PI * wheelRadius), m_motor(motor), m_encoder(encoder) {
+    : m_wheelCircumference(2 * PI * wheelRadius), 
+      m_motor(motor),
+      m_encoder(encoder),
+      m_targetVelocity(0),
+      m_previousError(0),
+      m_errorIntegral(0),
+      m_errorDerivative(0),
+      m_proportionalGain(1), 
+      m_derivativeGain(0),
+      m_integralGain(0) {
 }
 
 void PIDControlledMotor::Update() {
+    float currentVelocity = m_encoder->RevolutionsPerSecond() * m_wheelCircumference;
+    float error = m_targetVelocity - currentVelocity;
+    m_errorDerivative = error - m_previousError;
+    m_errorIntegral += error;
+    m_previousError = error;
 
+    int control = m_proportionalGain * error 
+                  + m_integralGain * m_errorIntegral 
+                  + m_derivativeGain * m_errorDerivative;
+    
+
+    m_motor.SetSpeed(control);
 }
--- arduino/lib/motor_control/PIDControlledMotor.h
+++ arduino/lib/motor_control/PIDControlledMotor.h
@@ -8,14 +8,21 @@ class PIDControlledMotor
 {
 public:
     PIDControlledMotor(const unsigned int wheelRadius, const Motor& motor, Encoder* const encoder);
-    void SetVelocity(const int velocity) { m_targetVelocity = velocity; }
+    void SetVelocity(const float velocity) { m_targetVelocity = velocity; }
     void Update();
 
 private:
-    int             m_targetVelocity;
     const float     m_wheelCircumference;
     Motor           m_motor;
     Encoder* const  m_encoder;
+
+    float           m_targetVelocity;
+    float           m_previousError;
+    float           m_errorIntegral;
+    float           m_errorDerivative;
+    const float     m_proportionalGain;
+    const float     m_derivativeGain;
+    const float     m_integralGain;
 
 };
 
