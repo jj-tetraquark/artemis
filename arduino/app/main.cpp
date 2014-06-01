d0953877f482779bf76b0ed8fcd7ae5362fceb77
jonnydark <jonny@jonnydark.com>
2014-06-01 18:42:39 +0100

    Updated the main application. This should make the robot drive in a circle in a steady rate

--- arduino/app/main.cpp
+++ arduino/app/main.cpp
@@ -1,29 +1,35 @@
 #include <Arduino.h>
-#include "Motor.h"
+#include "PIDControlledMotor.h"
 #include "TwoWheelOdometryManager.h"
+#include "TwoWheelMotorController.h"
 #include "RotaryEncoder.h"
+#include "Config.h"
 
-Motor leftMotor(2,3);
-Motor rightMotor(8,9);
+RotaryEncoder<LEFT> leftEncoder(Config::Pins::LEFT_ENCODER_A, 
+                                Config::Pins::LEFT_ENCODER_B, 
+                                Config::ROTARY_ENCODER_RATIO);
 
+RotaryEncoder<RIGHT> rightEncoder(Config::Pins::RIGHT_ENCODER_A,
+                                  Config::Pins::RIGHT_ENCODER_B,
+                                  Config::ROTARY_ENCODER_RATIO);
+
+PIDControlledMotor leftMotor(Config::Dimensions::WHEEL_RADIUS, 
+                             Motor(Config::Pins::LEFT_MOTOR_DIRECTION, Config::Pins::LEFT_MOTOR_SPEED),
+                             &leftEncoder);
+
+PIDControlledMotor rightMotor(Config::Dimensions::WHEEL_RADIUS,
+                              Motor(Config::Pins::RIGHT_MOTOR_DIRECTION, Config::Pins::RIGHT_MOTOR_SPEED),
+                              &rightEncoder);
+
+TwoWheelMotorController motorController(&leftMotor, &rightMotor, Config::Dimensions::ROBOT_WIDTH);
 
 void loop() {
-    for(int speed = 0; speed < 100; speed++) {
-        delay(5);
-        leftMotor.SetSpeed(speed);
-        rightMotor.SetSpeed(speed);
-    }
-    for(int speed = 100; speed > 0; speed--) {
-        delay(5);
-        leftMotor.SetSpeed(speed);
-        rightMotor.SetSpeed(speed);
-    }
+    motorController.OnTick();
 }
 
 int main()
 {
-    leftMotor.SetDirectionForwards();
-    rightMotor.SetDirectionBackwards();
+    motorController.SetVelocities(10, 0.2); // This should make the robot drive in a circle
     init();
     while(true)
         loop();
